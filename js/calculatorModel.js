fa5f38eaab4eabe419aa6ee7238062185852e29e
fomgleb <fomglib@gmail.com>
2023-06-04 17:27:32 +0300

    Move main logic to calculatorModel.js

--- js/calculatorModel.js
+++ js/calculatorModel.js
@@ -1,40 +1,176 @@
-const calculate = expressionString => {
-    let elements = expressionString.split(' ');
-    console.log(elements);
-
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].includes('!'))
-            elements[i] = Math.factorial(+elements[i].slice(0, -1))
-        else if (elements[i].includes('√'))
-            elements[i] = Math.sqrt(+elements[i].slice(2, -1))
+class Calculator extends EventTarget {
+    historyChangedEventName = 'historyChangedEvent'
+    #_history = ''
+    get #history() { return this.#_history }
+    set #history(value) {
+        this.#_history = value
+        this.dispatchEvent(new CustomEvent(this.historyChangedEventName, {
+            detail: { newHistory: this.#history }
+        }))
     }
 
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i] === '^') {
-            elements.splice(i - 1, 3, Math.pow(elements[i - 1], elements[i + 1]))
-            i--
+    inputChangedEventName = 'inputChangedEvent'
+    #_input = ''
+    get #input() { return this.#_input }
+    set #input(value) {
+        this.#_input = value
+        this.dispatchEvent(new CustomEvent(this.inputChangedEventName, {
+            detail: { newInput: this.#input }
+        }))
+    }
+
+    #_lastInputtedSymbol = ''
+    get #lastInputtedSymbol() { return this.#_lastInputtedSymbol }
+    set #lastInputtedSymbol(value) { this.#_lastInputtedSymbol = value }
+
+    inputDigit(digit) {
+        if (['!', '√'].includes(this.#lastInputtedSymbol)) {
+            this.#history = ''
+            this.#input = ''
+        } else if (this.#input == '0' || ['+', '-', '*', '÷', '^', '='].includes(this.#lastInputtedSymbol))
+            this.#input = ''
+        if (this.#lastInputtedSymbol == '=')
+            this.#history = ''
+        this.#input += digit
+        this.#lastInputtedSymbol = digit
+    }
+
+    inputOperator(operator) {
+        if (['+', '-', '*', '÷', '^'].includes(this.#lastInputtedSymbol)) {
+            this.#history = `${this.#history.slice(0, -2)}${operator} `
+            return
         }
-    }
-
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i] === '*' || elements[i] === '÷') {
-            const result = elements[i] === '*'
-                ? parseFloat(elements[i - 1]) * parseFloat(elements[i + 1])
-                : parseFloat(elements[i - 1]) / parseFloat(elements[i + 1])
-            elements.splice(i - 1, 3, result);
-            i--;
+        if (this.#lastInputtedSymbol == '=') {
+            this.#history = this.#input
+            this.#history = ''
         }
+        this.#history += ['!', '√'].includes(this.#lastInputtedSymbol)
+        ? ` ${operator} `
+        : `${this.#input} ${operator} `
+        this.#input = this.#calculate(this.#history.slice(0, -3))
+        this.#lastInputtedSymbol = operator
     }
 
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i] === '+' || elements[i] === '-') {
-            const result = elements[i] === '+'
-                ? parseFloat(elements[i - 1]) + parseFloat(elements[i + 1])
-                : parseFloat(elements[i - 1]) - parseFloat(elements[i + 1])
-            elements.splice(i - 1, 3, result);
-            i--;
+    clearAll() {
+        this.#history = ''
+        this.#input = '0'
+        this.#lastInputtedSymbol = 'C'
+    }
+
+    clearOne() {
+        if (this.#input != '0')
+            this.#input = this.#input.slice(0, -1)
+        if (this.#input == '')
+            this.#input = '0'
+        this.#lastInputtedSymbol = '⌫'
+    }
+
+    changeInputSign() {
+        if (['=', '!', '√'].includes(this.#lastInputtedSymbol))
+            this.#history = ''
+        if (this.#input == '0')
+            return
+        if (this.#input[0] != '-')
+            this.#input = `-${this.#input}`
+        else
+            this.#input = this.#input.slice(1)
+        this.#lastInputtedSymbol = '±'
+    }
+
+    inputPoint() {
+        if (['+', '-', '*', '÷', '=', '^'].includes(this.#lastInputtedSymbol)) {
+            this.#input = '0'
+            if (this.#lastInputtedSymbol == '=') this.#history = ''
+        } else if (this.#input.includes('.'))
+            return
+        this.#input += '.'
+        this.#lastInputtedSymbol = '.'
+    }
+
+    inputEqual() {
+        if (this.#lastInputtedSymbol == '=')
+            return
+        if (['!', '√'].includes(this.#lastInputtedSymbol)) {
+            this.#input = this.#calculate(this.#history)
+        } else {
+            this.#history += this.#input
+            this.#input = this.#calculate(this.#history)
         }
+        this.#history += ' ='
+        this.#lastInputtedSymbol = '='
     }
 
-    return elements[0];
+    inputPercent() {
+        if (this.#lastInputtedSymbol == '=')
+            return
+        this.#input = this.#calculate(this.#history.slice(0, -3)) * this.#input / 100.0
+        this.#lastInputtedSymbol = '%'
+    }
+
+    inputFactorial() {
+        if (this.#lastInputtedSymbol == '!')
+            return
+        if (this.#lastInputtedSymbol == '=') {
+            this.#history = `${this.#input}!`
+            this.#input = this.#calculate(this.#history)
+        } else {
+            this.#history += `${this.#input}!`
+            this.#input = this.#calculate(this.#history)
+        }
+        this.#lastInputtedSymbol = '!'
+    }
+
+    inputRoot() {
+        if (this.#lastInputtedSymbol == '√')
+            return
+        if (this.#lastInputtedSymbol == '=') {
+            this.#history = `√(${this.#input})`
+            this.#input = this.#calculate(this.#history)
+        } else {
+            this.#history += `√(${this.#input})`
+            this.#input = this.#calculate(this.#history)
+        }
+        this.#lastInputtedSymbol = '√'
+    }
+
+    #calculate = expressionString => {
+        let elements = expressionString.split(' ');
+        console.log(elements);
+
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].includes('!'))
+                elements[i] = Math.factorial(+elements[i].slice(0, -1))
+            else if (elements[i].includes('√'))
+                elements[i] = Math.sqrt(+elements[i].slice(2, -1))
+        }
+
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i] === '^') {
+                elements.splice(i - 1, 3, Math.pow(elements[i - 1], elements[i + 1]))
+                i--
+            }
+        }
+
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i] === '*' || elements[i] === '÷') {
+                const result = elements[i] === '*'
+                    ? parseFloat(elements[i - 1]) * parseFloat(elements[i + 1])
+                    : parseFloat(elements[i - 1]) / parseFloat(elements[i + 1])
+                elements.splice(i - 1, 3, result);
+                i--;
+            }
+        }
+
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i] === '+' || elements[i] === '-') {
+                const result = elements[i] === '+'
+                    ? parseFloat(elements[i - 1]) + parseFloat(elements[i + 1])
+                    : parseFloat(elements[i - 1]) - parseFloat(elements[i + 1])
+                elements.splice(i - 1, 3, result);
+                i--;
+            }
+        }
+
+        return elements[0];
+    }
 }
--- js/calculatorViewModel.js
+++ js/calculatorViewModel.js
@@ -8,123 +8,27 @@ class CalculatorViewModel {
     #_outputFieldElement = this.#calculatorContainerElement.querySelector('.output-field')
     get #outputFieldElement() { return this.#_outputFieldElement }
 
-    #_lastInputtedSymbol = ''
-    get #lastInputtedSymbol() { return this.#_lastInputtedSymbol }
-    set #lastInputtedSymbol(value) { this.#_lastInputtedSymbol = value }
+    #_calculator = new Calculator()
+    get #calculator() { return this.#_calculator }
 
-    get #history() { return this.#historyFieldElement.textContent }
-    set #history(value) { this.#historyFieldElement.textContent = value }
+    constructor() {
+        this.#calculator.addEventListener(this.#calculator.inputChangedEventName, event => {
+            this.#outputFieldElement.textContent = event.detail.newInput
+        })
 
-    get #output() { return this.#_outputFieldElement.textContent }
-    set #output(value) { this.#outputFieldElement.textContent = value }
-
-    digitButtonClicked(digit) {
-        if (['!', '√'].includes(this.#lastInputtedSymbol)) {
-            this.#history = ''
-            this.#output = ''
-        } else if (this.#output == '0' || ['+', '-', '*', '÷', '^', '='].includes(this.#lastInputtedSymbol))
-            this.#output = ''
-        if (this.#lastInputtedSymbol == '=')
-            this.#history = ''
-        this.#output += digit
-        this.#lastInputtedSymbol = digit
+        this.#calculator.addEventListener(this.#calculator.historyChangedEventName, event => {
+            this.#historyFieldElement.textContent = event.detail.newHistory
+        })
     }
 
-    operatorButtonClicked(operator) {
-        if (['+', '-', '*', '÷', '^'].includes(this.#lastInputtedSymbol)) {
-            this.#history = `${this.#history.slice(0, -2)}${operator} `
-            return
-        }
-        if (this.#lastInputtedSymbol == '=') {
-            this.#history = this.#output
-            this.#history = ''
-        }
-        this.#history += ['!', '√'].includes(this.#lastInputtedSymbol)
-        ? ` ${operator} `
-        : `${this.#output} ${operator} `
-        this.#output = calculate(this.#history.slice(0, -3))
-        this.#lastInputtedSymbol = operator
-    }
-
-    clearButtonClicked() {
-        this.#history = ''
-        this.#output = '0'
-        this.#lastInputtedSymbol = 'C'
-    }
-
-    backspaceButtonClicked() {
-        if (this.#output != '0')
-            this.#output = this.#output.slice(0, -1)
-        if (this.#output == '')
-            this.#output = '0'
-        this.#lastInputtedSymbol = '⌫'
-    }
-
-    changeSignButtonClicked() {
-        if (['=', '!', '√'].includes(this.#lastInputtedSymbol))
-            this.#history = ''
-        if (this.#output == '0')
-            return
-        if (this.#output[0] != '-')
-            this.#output = `-${this.#output}`
-        else
-            this.#output = this.#output.slice(1)
-        this.#lastInputtedSymbol = '±'
-    }
-
-    pointButtonClicked() {
-        if (['+', '-', '*', '÷', '=', '^'].includes(this.#lastInputtedSymbol)) {
-            this.#output = '0'
-            if (this.#lastInputtedSymbol == '=') this.#history = ''
-        } else if (this.#output.includes('.'))
-            return
-        this.#output += '.'
-        this.#lastInputtedSymbol = '.'
-    }
-
-    equalButtonClicked() {
-        if (this.#lastInputtedSymbol == '=')
-            return
-        if (['!', '√'].includes(this.#lastInputtedSymbol)) {
-            this.#output = calculate(this.#history)
-        } else {
-            this.#history += this.#output
-            this.#output = calculate(this.#history)
-        }
-        this.#history += ' ='
-        this.#lastInputtedSymbol = '='
-    }
-
-    percentButtonClicked() {
-        if (this.#lastInputtedSymbol == '=')
-            return
-        this.#output = calculate(this.#history.slice(0, -3)) * this.#output / 100.0
-        this.#lastInputtedSymbol = '%'
-    }
-
-    factorialButtonClicked() {
-        if (this.#lastInputtedSymbol == '!')
-            return
-        if (this.#lastInputtedSymbol == '=') {
-            this.#history = `${this.#output}!`
-            this.#output = calculate(this.#history)
-        } else {
-            this.#history += `${this.#output}!`
-            this.#output = calculate(this.#history)
-        }
-        this.#lastInputtedSymbol = '!'
-    }
-
-    rootButtonClicked() {
-        if (this.#lastInputtedSymbol == '√')
-            return
-        if (this.#lastInputtedSymbol == '=') {
-            this.#history = `√(${this.#output})`
-            this.#output = calculate(this.#history)
-        } else {
-            this.#history += `√(${this.#output})`
-            this.#output = calculate(this.#history)
-        }
-        this.#lastInputtedSymbol = '√'
-    }
+    digitButtonClicked(digit) { this.#calculator.inputDigit(digit) }
+    operatorButtonClicked(operator) { this.#calculator.inputOperator(operator) }
+    clearButtonClicked() { this.#calculator.clearAll() }
+    backspaceButtonClicked() { this.#calculator.clearOne() }
+    changeSignButtonClicked() { this.#calculator.changeInputSign() }
+    pointButtonClicked() { this.#calculator.inputPoint() }
+    equalButtonClicked() { this.#calculator.inputEqual() }
+    percentButtonClicked() { this.#calculator.inputPercent() }
+    factorialButtonClicked() { this.#calculator.inputFactorial() }
+    rootButtonClicked() { this.#calculator.inputRoot() }
 }
